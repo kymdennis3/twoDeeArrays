b25d124a6cba34e4a6a3fec55d3ab93fa28a1398
Kimtai <kymdennis3@gmail.com>
2023-01-11 07:24:43 +0300

    An optional way of writing the 2D array

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
+
+        String[][] cars = {
+                            {"Ford","Toyota", "Nissan"},
+                            {"Lambo", "Mustang", "Tesla"},
+                            {"Ferarri", "Benzo", "Beamer"}
+                          };
+
+
+        for (int i = 0; i < cars.length; i++) {
+            System.out.println();
+            for (int j = 0; j < cars[i].length; j++) {
+                System.out.println(cars[i][j]+" ");
+            }
+
+        }
+    }}
